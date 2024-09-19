14ba3e39e5a1fcb971fd2073417c8492cbd32867
chirag025 <cg75187@gmail.com>
2024-09-20 03:56:19 +0530

    Update Header.jsx

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,10 +1,10 @@
 import React, { useContext } from "react";
 import { SunIcon, MoonIcon } from "@heroicons/react/solid";
 import { AppContext } from "../context/AppContext";
+import { Link } from "react-router-dom";
 
 const Header = () => {
-
-  const {isDarkMode, setIsDarkMode} = useContext(AppContext);
+  const { isDarkMode, setIsDarkMode } = useContext(AppContext);
 
   const toggleDarkMode = () => {
     setIsDarkMode(!isDarkMode);
@@ -14,13 +14,14 @@ const Header = () => {
   return (
     <div className="w-full bg-white dark:bg-gray-800 border-b dark:border-gray-700 shadow-md py-3 fixed top-0 left-0 z-10">
       <header className="flex justify-between md:justify-center items-center w-full max-w-6xl mx-auto px-4">
-        <h1 className="text-2xl font-bold uppercase text-black dark:text-white">
-          Blog Nexus
-        </h1>
+        <Link to="/">
+          <h1 className="text-2xl font-bold uppercase text-black dark:text-white">
+            Blog Nexus
+          </h1>
+        </Link>
         <button
           onClick={toggleDarkMode}
-          className="py-2 px-4 bg-blue-600 text-white text-sm font-semibold rounded-md hover:bg-blue-700 transition duration-300 dark:bg-gray-700 dark:hover:bg-gray-600 absolute right-6"
-        >
+          className="py-2 px-4 bg-blue-600 text-white text-sm font-semibold rounded-md hover:bg-blue-700 transition duration-300 dark:bg-gray-700 dark:hover:bg-gray-600 absolute right-6">
           {isDarkMode ? (
             <SunIcon className="h-6 w-6 text-yellow-400" />
           ) : (
